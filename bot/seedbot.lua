6e4633212e46f52d2e36d15798280f6f56049cd1
LordError <LordErrorBot@gmail.com>
2016-03-11 01:37:09 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {175349963},--Sudo usersday
+    sudo_users = {175349963},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
